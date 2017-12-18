211f4f0f35b0d01b1685ce3271325f2289d48842
Pro Retina <proretina@APPLEs-MacBook-Pro.local>
2017-12-18 19:17:46 +0530

    my first and final commit

--- JsonParsingSwift4/ViewController.swift
+++ JsonParsingSwift4/ViewController.swift
@@ -8,11 +8,66 @@
 
 import UIKit
 
+struct WebsiteDescription:Decodable {
+    let name:String
+    let description:String
+    let courses:[Course]
+}
+
+struct Course:Decodable {
+    let id:Int?
+    let name:String?            // make optional ? if field is comming null
+    let link:String?
+    let imageUrl:String?
+//
+//    init(json:[String:Any]){
+//        id = json["id"] as? Int ?? -1
+//        name = json["name"] as? String ?? ""
+//        link = json["link"] as? String ?? ""
+//        imageURL = json["imageUrl"] as? String ?? ""
+//    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses" or course
+//        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields" //missing field
+        guard let url = URL(string: jsonUrlString) else{ return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // perhapse check err
+            // also perhapse chek response status 200 ok
+            
+            guard let data = data else{ return}
+//            let dataAsString = String(data: data, encoding: .utf8)
+//            print(dataAsString)
+            
+            do {
+                
+                
+                
+//                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+//                print(websiteDescription.name, websiteDescription.description)
+                
+                let courses = try JSONDecoder().decode([Course].self, from: data) // add only []
+//                print(courses)
+                print(courses[0].name!)
+                
+                // Swift 2/3/ObjC
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] else{ return }
+//                let course = Course(json:json)
+//                print(course.imageURL)
+            }catch let jsonErr {
+                print("Error serializing json: ",jsonErr)
+            }
+            
+        }.resume()
+        
+//        let mycourse = Course(id:1,name:"my course", link: "some link", imageURL: "some image url")
+//        print(mycourse)
     }
 
     override func didReceiveMemoryWarning() {
